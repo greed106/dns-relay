067bfb3e24f1d08fe84e3522bae0642415c8c672
Yang Mingjia <130547678+greed106@users.noreply.github.com>
2024-06-29 05:55:54 +0800

    添加了优雅的退出

--- src/main.c
+++ src/main.c
@@ -3,6 +3,14 @@
 #include "dns_server.h"
 #include <signal.h>
 
+// 全局变量，用于在清理函数中访问DNS服务器实例
+dns_server_t server;
+
+// 清理函数，用于释放资源
+static void cleanup(int status) {
+    dns_server_stop(&server);
+}
+
 int main(int argc, char **argv) {
     // 服务器的配置信息
     struct Config server_config = {0};
@@ -10,12 +18,14 @@ int main(int argc, char **argv) {
     parse_args(argc, argv, &server_config);
     init_logger(&server_config);
 
-    dns_server_t server;
+    // 注册清理函数
+    signal(SIGINT, cleanup);
+
     if(dns_server_init(&server, &server_config) != 0) {
         hloge("Failed to initialize DNS Server");
         return -1;
     }
-    signal(SIGINT, dns_server_stop);
+
     dns_server_start(&server);
 
     return 0;
